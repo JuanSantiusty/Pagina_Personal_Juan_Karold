dfebd1bb9d2ce1ee3705cd52897eefae19174eb9
JuanDev <juanmoran@unicauca.edu.co>
2025-09-13 17:52:50 -0500

    entidade y repos

---
+++ sin_framework/js/capaAccesoADatos/entidades/contacto.js
@@ -0,0 +1,9 @@
+class Contacto{
+    constructor(id,nombre,correo,telefono,asunto){
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.asunto = asunto;
+    }
+}
---
+++ sin_framework/js/capaAccesoADatos/repositorios/contactoRepository.js
@@ -0,0 +1,38 @@
+class ContactoRepository {
+    constructor(){
+        this.repo = [];
+    }
+    getAll(){
+        return this.repo;
+    } 
+    getById(id){
+        return this.repo.find(c => c.id === id);
+    } 
+    add(contacto) {
+        this.repo.push(contacto);
+    }
+    update(contacto){
+        if((indice = this.indiceElemento(contacto.id)) > 0){
+            this.repo[indice] = contacto;
+        }
+    }
+    remove(id){
+        if((indice = this.indiceElemento(id) > 0)){
+            this.repo.splice(indice,1);
+        }
+    }
+    clear(){
+        this.repo.clear();
+    }
+
+    indiceElemento(id){
+        indice = -1;
+        for(i=0;i<this.repo.length;i++){
+            if(this.repo[i].id === id){
+                indice = i;
+                break;
+            }
+        }
+        return indice;
+    }
+}
